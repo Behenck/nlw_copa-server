ad562ecfc89609474c481768e89c28cd665c315b
Denilson Behenck <denilsontrespa10@gmail.com>
2022-11-16 17:06:53 -0300

    project server nlwcopa

---
+++ src/server.ts
@@ -0,0 +1,33 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
+
+import { poolRoutes } from './routes/pools'
+import { guessRoutes } from './routes/guess'
+import { gameRoutes } from './routes/game'
+import { userRoutes } from './routes/user'
+import { authRoutes } from './routes/auth'
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  })
+
+  await fastify.register(cors, {
+    origin: true, // depois colocar por exemplo www.denilson.com.br
+  })
+
+  await fastify.register(jwt, {
+    secret: 'abc3358b7afee595016baab36857a0fa', // .env
+  })
+
+  await fastify.register(poolRoutes)
+  await fastify.register(authRoutes)
+  await fastify.register(guessRoutes)
+  await fastify.register(gameRoutes)
+  await fastify.register(userRoutes)
+
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
+}
+
+bootstrap()
